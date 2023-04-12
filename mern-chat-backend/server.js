54f15099019b1a6c9d0d73088cc8ad026d204b77
Aubrey LI <ashin407489345@gmail.com>
2023-04-13 02:34:16 +0800

    remove the last commit

--- mern-chat-backend/server.js
+++ mern-chat-backend/server.js
@@ -46,8 +46,7 @@ const server = http.createServer(app);
 const io = require('socket.io')(server, {
     // will not have any cross origin errors while building our app
     cors: {
-        // origin: 'http://localhost:3000',
-        origin: 'https://mern-chatapp-client.onrender.com',
+        origin: 'http://localhost:3000',
         methods: ['GET', 'POST']
     }
 });
