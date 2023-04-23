554cd9aba80c03a6228a7bd1955d10d21b56807f
Aubrey LI <ashin407489345@gmail.com>
2023-04-24 02:10:32 +0800

    serverUrl

--- mern-chat-frontend/src/constant/server.js
+++ mern-chat-frontend/src/constant/server.js
@@ -1,4 +1,4 @@
-const SOCKET_URL = 'http://localhost:5000';
-// const SOCKET_URL = 'https://mern-chatapp-server.onrender.com';
+// const SOCKET_URL = 'http://localhost:5000';
+const SOCKET_URL = 'https://mern-chatapp-server.onrender.com';
 
 export { SOCKET_URL };
